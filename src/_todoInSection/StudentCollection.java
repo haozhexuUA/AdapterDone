c501b4f356451fe0d854bdf970589a1a4c95413d
Haozhe Xu <haozhexu@email.arizona.edu>
2016-10-14 14:12:15 -0700

    Update StudentCollection.java

--- src/_todoInSection/StudentCollection.java
+++ src/_todoInSection/StudentCollection.java
@@ -3,63 +3,144 @@ package _todoInSection;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+
 import model.Student;
 
 /**
  * StudentCollection is a collection class that keeps track of all our students.
  * 
- * @author Jorge Vergara, Rick Mercer, and Jeremy Mowery (jermowery@email.arizona.edu)
- *    
+ * @author Jorge Vergara, Rick Mercer, and Jeremy Mowery
+ *         (jermowery@email.arizona.edu)
+ * 
  */
 // TODO 1: Adapt this class to a TableModel with the methods JTable needs.
 // You can Google the methods or let Eclipse write the method stubs for you
 // (after adding implements ListModel to the class heading).
 //
 // Note: Some ListModel need not be implemented.
-public class StudentCollection  {
+public class StudentCollection implements TableModel {
 
-  private List<Student> theStudents;
+	private List<Student> theStudents;
 
-  /**
-   * The constructor for a StudentCollection. Initializes the list of
-   * students.
-   */
-  public StudentCollection() {
-    theStudents = new ArrayList<Student>();
-    setUpDefaultList();
-  }
+	/**
+	 * The constructor for a StudentCollection. Initializes the list of
+	 * students.
+	 */
+	public StudentCollection() {
+		theStudents = new ArrayList<Student>();
+		setUpDefaultList();
+	}
 
-  /**
-   * Initializes the list with a few hard-coded students
-   */
-  private void setUpDefaultList() {
-    theStudents.add(new Student("Kim", "Computer Science", 4.0, 23));
-    theStudents.add(new Student("Chris", "Computer Science", 4.0, 23));
-    theStudents.add(new Student("Devon", "Math", 2.7, 21));
-    theStudents.add(new Student("Ashley", "Biology", 3.29, 27));
-    theStudents.add(new Student("Taylor", "Economics", 3.9, 25));
-    theStudents.add(new Student("Chris", "Economics", 3.8, 19));
-    theStudents.add(new Student("Dakota", "Psychology", 1.99, 22));
-  }
- 
-  public int size() {
-    return theStudents.size();
-  }
+	/**
+	 * Initializes the list with a few hard-coded students
+	 */
+	private void setUpDefaultList() {
+		theStudents.add(new Student("Kim", "Computer Science", 4.0, 23));
+		theStudents.add(new Student("Chris", "Computer Science", 4.0, 23));
+		theStudents.add(new Student("Devon", "Math", 2.7, 21));
+		theStudents.add(new Student("Ashley", "Biology", 3.29, 27));
+		theStudents.add(new Student("Taylor", "Economics", 3.9, 25));
+		theStudents.add(new Student("Chris", "Economics", 3.8, 19));
+		theStudents.add(new Student("Dakota", "Psychology", 1.99, 22));
+	}
 
-  public void add(Student newStudent) {
-    theStudents.add(newStudent);
-  }
+	public int size() {
+		return theStudents.size();
+	}
 
-  public Student get(int index) {
-    return theStudents.get(index);
-  }
+	public void add(Student newStudent) {
+		theStudents.add(newStudent);
+	}
 
-  public Student getFirstStudentWithName(String name) {
-    for (int index = 0; index < size(); index++) {
-      if (name.equals(theStudents.get(index).getName()))
-        return theStudents.get(index);
-    }
-    return null; // not found
-  }
+	public Student get(int index) {
+		return theStudents.get(index);
+	}
+
+	public Student getFirstStudentWithName(String name) {
+		for (int index = 0; index < size(); index++) {
+			if (name.equals(theStudents.get(index).getName()))
+				return theStudents.get(index);
+		}
+		return null; // not found
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return this.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 4;
+	}
+
+	@Override
+	public String getColumnName(int index) {
+		if(index ==0)
+			return "Name";
+		if(index ==1)
+			return "Major";
+		else if(index ==2)
+			return "GPA";
+		else
+		 return	"Age";
+	}
+
+	// String String double int
+	// name major gpa age
+	// 0    1     2   3
+	@Override
+	public Class<?> getColumnClass(int index) {
+		if(index <=1)
+			return String.class;
+		else if(index ==2)
+			return Double.class;
+		else
+		 return	Integer.class;
+	}
+
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Student temp = theStudents.get(rowIndex);
+		
+		if(columnIndex ==0)
+			return temp.getName();
+		if(columnIndex==1)
+			return temp.getMajor();
+		if(columnIndex==2)
+			return temp.getGPA();
+		
+		else
+			return temp.getAge();
+	     
+	}
+
+	@Override
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addTableModelListener(TableModelListener l) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeTableModelListener(TableModelListener l) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
